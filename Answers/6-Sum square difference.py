2820e0a305d38d4ddd26c4bcb8d658dbc8dca996
muaet <muhammet.topcu@hotmail.com>
2022-09-04 23:10:51 +0300

    Project euler answers

--- Answers/6-Sum square difference.py
+++ Answers/6-Sum square difference.py
@@ -1,17 +1,21 @@
-#The sum of the squares of the first ten natural numbers is,
-#1^2 + 2^2 + ... + 10^2 = 385
-#The square of the sum of the first ten natural numbers is,
-#(1 + 2 + ... + 10)^2 = 55^2 = 3025
-#Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
-#Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
-def sum_of_squares(sayi):
-    sum2=0
-    sum1=0
-    for i in range(sayi+1):
-        sum1+=i*i
-    for i in range(sayi+1):
-        sum2+=i
-    sum2=sum2*sum2
-    return sum2-sum1
+# The sum of the squares of the first ten natural numbers is,
+# 1^2 + 2^2 + ... + 10^2 = 385
+# The square of the sum of the first ten natural numbers is,
+# (1 + 2 + ... + 10)^2 = 55^2 = 3025
+# Hence the difference between the sum of the squares of the first ten natural numbers
+# and the square of the sum is 3025 − 385 = 2640.
+# Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+
+def sum_of_squares(number):
+    sum2 = 0
+    sum1 = 0
+    for i in range(number + 1):
+        sum1 += i * i
+    for i in range(number + 1):
+        sum2 += i
+    sum2 = sum2 * sum2
+    return sum2 - sum1
+
 
 print(sum_of_squares(100))
--- Answers/7-10001st prime.py
+++ Answers/7-10001st prime.py
@@ -1,21 +1,19 @@
-#By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
-#What is the 10 001st prime number?
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+# What is the 10 001st prime number?
 
-import time
-start=time.time()
-prime=[2]
 
-def kacinci_prime(sayi):
-    for a in range(2,56161846123184318543):
+def which_prime(number):
+    prime = [2]
+    for a in range(2, 10000000000000000000):
         for b in prime:
-            if a%b==0:
+            if a % b == 0:
                 break
-            if a%b!=0 and b==prime[-1]:
+            if a % b != 0 and b == prime[-1]:
                 prime.append(a)
                 break
-        if len(prime)==sayi:
-            return(prime[-1])
+        if len(prime) == number:
             break
-print(kacinci_prime(10001))
-end=time.time()
-print(end-start)
+    return prime[-1]
+
+
+print(which_prime(10001))
