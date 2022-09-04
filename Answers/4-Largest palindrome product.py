10d96c77bd71e9e964d0c12b07c0f86fce091eef
muaet <muhammet.topcu@hotmail.com>
2022-09-04 22:41:16 +0300

    Project euler answers

--- Answers/4-Largest palindrome product.py
+++ Answers/4-Largest palindrome product.py
@@ -1,39 +1,36 @@
-# A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
-# Find the largest palindrome made from the product of two 3-digit numbers.
-# birler
-# za=(984%10)//1
-# onlar
-# za=(984%100)//10
-# yüzler
-# za=(984%1000)//100
-# binler
-# za=(8984%10000)//1000
-import time
-start=time.time()
-def polindrom_test(sayi):  # polindrom olup olmadığını kontrol eder.
-    # True ya da False.
-    uzunluk = len(str(sayi))
-    for i in range((uzunluk) // 2):
-        if str(sayi)[i] == str(sayi)[-i - 1]:
+# A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is
+# 9009 = 91 × 99. Find the largest palindrome made from the product of two 3-digit numbers.
+
+
+def is_palindrome(number):
+    """
+    Checks if a number is palindrome or not?
+    @param number: Number to be checked
+    @return: Boolean
+    """
+    for i in range(len(str(number)) // 2):
+        if str(number)[i] == str(number)[-i - 1]:
             continue
         else:
             return False
     return True
 
 
-def two_three_digits():  # 3 basamaklı iki sayının çarpımı olarak bulmak
+def two_three_digits():
+    """
+    Find palindromes as a product of two three digits numbers.
+    @return: List
+    """
     palindrome_list = []
     for a in range(100, 1000):
         for b in range(100, 1000):
-            canditate = a * b
-            if polindrom_test(canditate):
+            candidate = a * b
+            if is_palindrome(candidate):
                 palindrome_list.append(a * b)
             else:
                 continue
     palindrome_list.sort()
-    return palindrome_list[-1]
+    return palindrome_list
 
 
-print(two_three_digits())
-end=time.time()
-print(end-start)
+print(two_three_digits()[-1])
